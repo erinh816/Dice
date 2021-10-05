a277501cf090f9d582424444afec6e0de8126465
Silu Hu <erinh816@gmail.com>
2021-10-05 10:32:48 -0400

    everything but shaky animation

--- dice/src/RollDice.js
+++ dice/src/RollDice.js
@@ -1,8 +1,14 @@
 //parent component
 import React, { Component } from "react";
 import Dice from "./Dice";
+import "./RollDice.css";
 
 export default class RollDice extends Component {
+  //we can do static defaultProps or just define the array in the function
+  //   static defaultProps = {
+  //     faces: ["one", "two", "three", "four", "five", "six"],
+  //   };
+
   constructor(props) {
     super(props);
     this.state = {
@@ -11,21 +17,24 @@ export default class RollDice extends Component {
       dice1: "one",
       dice2: "one",
     };
+    //this.bind is not necessary because I used arrow function rollDice
+    // this.tollDice = this.rollDice.bind(this);
   }
 
   rollDice = () => {
     let faces = ["one", "two", "three", "four", "five", "six"];
     let item = faces[Math.floor(Math.random() * faces.length)];
     let item2 = faces[Math.floor(Math.random() * faces.length)];
-    this.setState({ dice1: item });
-    this.setState({ dice2: item2 });
+    this.setState({ dice1: item, dice2: item2 });
   };
 
   render() {
     return (
-      <div>
-        <Dice face={this.state.dice1} />
-        <Dice face={this.state.dice2} />
+      <div className="RollDice">
+        <div className="RollDice-container">
+          <Dice face={this.state.dice1} />
+          <Dice face={this.state.dice2} />
+        </div>
         <button onClick={this.rollDice}>ROLL</button>
       </div>
     );
